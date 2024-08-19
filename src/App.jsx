62e8d6203854cc71e9c4a9f7180ff0416344c9ee
Efty <efty131331@gmail.com>
2024-08-19 17:35:34 +0600

    2nd

--- src/App.jsx
+++ src/App.jsx
@@ -8,31 +8,18 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+     
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-
+       
+</div>
       {/* Video Player Section */}
       <div className="video-container">
-        <video controls width="600">
-          <source src="path-to-your-video.mp4" type="video/mp4" />
+        <video controls controlsList="nodownload" width="600">
+          <source src="/src/assets/video.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
       </div>

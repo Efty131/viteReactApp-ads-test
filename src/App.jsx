c78f86ebc509254e0e09f999620c0851d2cc8b95
Efty <efty131331@gmail.com>
2024-08-20 18:21:12 +0600

    social-again

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import BlogPost from './blogPost';
 import NativeBanner from './nativeBanner';
 import Banner300 from './banner300x250';
 
 function App() {
+  useEffect(() => {
+    // Create the script element
+    const script = document.createElement('script');
+    script.src = '//pl24100074.highratecpm.com/9e/fc/bf/9efcbfb4d352aab8d10b6c44a802b500.js';
+    script.type = 'text/javascript';
+
+    // Append the script to the body
+    document.body.appendChild(script);
+
+    // Clean up the script when the component is unmounted
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
   return (
     <>
       <BlogPost />

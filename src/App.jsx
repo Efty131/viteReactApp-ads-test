74226597545199be9e370be3a2b5c1425aae5a7b
Efty <efty131331@gmail.com>
2024-08-20 18:12:55 +0600

    huh2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import BlogPost from './blogPost';
 import NativeBanner from './nativeBanner';
+import AdsterraAD from './adsterraAd';
 
 function App() {
   return (
     <>
       <BlogPost />
       <NativeBanner />
+      <AdsterraAD />
     </>
   );
 }

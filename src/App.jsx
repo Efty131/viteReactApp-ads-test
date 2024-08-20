e0efc2a87e425dcc0c741b9c21a6a68a91dbaf8f
Efty <efty131331@gmail.com>
2024-08-20 18:17:10 +0600

    shit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import BlogPost from './blogPost';
 import NativeBanner from './nativeBanner';
-import SocialBar from './socialBar';
+import Banner300 from './banner300x250';
 
 function App() {
   return (
     <>
       <BlogPost />
       <NativeBanner />
-      <SocialBar />
+      <Banner300 />
     </>
   );
 }

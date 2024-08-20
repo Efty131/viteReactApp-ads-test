033306ac02e89cef19eb31dfe96f99c9cbde11e4
Efty <efty131331@gmail.com>
2024-08-20 17:14:01 +0600

    nativeBanner

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import BlogPost from './blogPost';
+import Banner720 from './banner720';
+import NativeBanner from './nativeBanner';
 
 function App() {
   return (
     <>
+    <Banner720 />
      <h1 style={{textAlign: 'center'}}>App.jsx</h1>
       <BlogPost />
+      <NativeBanner />
     </>
   );
 }
---
+++ src/nativeBanner.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect } from 'react';
+
+const NativeBanner = () => {
+  useEffect(() => {
+    // Create the script element for the native banner ad
+    const script = document.createElement('script');
+    script.async = true;
+    script.setAttribute('data-cfasync', 'false');
+    script.src = '//pl24099918.highratecpm.com/d3c8996efea361b31704642e35e4f89a/invoke.js';
+    
+    // Append the script to the div with the ID where the ad will be rendered
+    const container = document.getElementById('container-d3c8996efea361b31704642e35e4f89a');
+    container.appendChild(script);
+
+    // Clean up the script when the component is unmounted
+    return () => {
+      container.removeChild(script);
+    };
+  }, []);
+
+  return (
+    <div>
+        <p style={{textAlign: 'center'}}>Native Banner</p>
+      {/* This div will hold the native banner ad */}
+      <div id="container-d3c8996efea361b31704642e35e4f89a"></div>
+    </div>
+  );
+};
+
+export default NativeBanner;
